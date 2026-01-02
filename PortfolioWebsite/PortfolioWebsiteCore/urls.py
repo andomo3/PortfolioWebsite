ca60eea62485c98cef778be48aa20db4952f1017
[Abba Otieno Ndomo] <[andomo3@gatech.edu]>
2026-01-01 23:07:44 -0500

    final

--- PortfolioWebsite/PortfolioWebsiteCore/urls.py
+++ PortfolioWebsite/PortfolioWebsiteCore/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf.urls.static import static
-from django.conf import settings\nimport os
+from django.conf import settings
+import os
 from .serve_media import serve_resume
 
 
@@ -34,5 +35,13 @@ urlpatterns = [
 ]
 
 
-# ✅ Append this outside the urlpatterns list    
-if settings.DEBUG:\n    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])\n    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\n\nif os.getenv('RENDER'):\n    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\n
+# Append this outside the urlpatterns list
+if settings.DEBUG:
+    if getattr(settings, "STATICFILES_DIRS", None):
+        urlpatterns += static(
+            settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0]
+        )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if os.getenv("RENDER"):
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
